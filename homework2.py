e5b72965c1f9a0b789ed9d4c12cef4f5979c2fcd
YopterMatbEro <yopter2013@gmail.com>
2024-07-29 17:31:41 +0300

    variables

---
+++ homework2.py
@@ -0,0 +1,7 @@
+quantity = 12
+hours = 1.5
+course = 'Python'
+time_for_task = hours / quantity
+
+print(f'Python, всего задач: {quantity}, затрачено часов: {hours}, среднее время выполнения: {time_for_task} часа')
+print(f'Или {hours * 60 / quantity} минут')
