8c45329821670522d6d21f57560cb10f7f899766
YopterMatbEro <yopter2013@gmail.com>
2024-07-29 18:08:09 +0300

    tuple, list

---
+++ homework5.py
@@ -0,0 +1,14 @@
+immutable_var = ('Hello', ['World', '!'], 5,)
+print(immutable_var)
+
+try:
+    immutable_var[0] = 'Goodbye'
+    print(immutable_var)
+except TypeError as err:
+    print(f'\nОшибка: {err}\nТип переменной immutable_var: {type(immutable_var)} - неизменяемый тип)\n')
+
+mutable_list = [1, 2, 3, 'four']
+print(mutable_list)
+
+mutable_list[0] = 'один'
+print(mutable_list)
